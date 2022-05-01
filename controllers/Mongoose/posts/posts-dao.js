e868db14db78d7eece0e08bf55dc5be3faa0fa4c
Matamir <mhirsch217@gmail.com>
2022-05-01 18:31:39 -0400

    fix updatePost bug

--- controllers/Mongoose/posts/posts-dao.js
+++ controllers/Mongoose/posts/posts-dao.js
@@ -1,4 +1,5 @@
 import postsModel from "./posts-model.js";
 export const findAllPosts = () => postsModel.find();
 export const findPostById = (id) => postsModel.findById(id);
-export default {findAllPosts, findPostById};
+export const updatePost = (pid, post) => postsModel.updateOne({_id:pid},{$set:post});
+export default {findAllPosts, findPostById, updatePost};
