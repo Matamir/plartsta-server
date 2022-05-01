55a424451113170e925893b40a349ef0645df01d
Matamir <mhirsch217@gmail.com>
2022-05-01 13:49:11 -0400

    add mongodb stuff 2

--- server.js
+++ server.js
@@ -7,10 +7,9 @@ import userController from './controllers/user-controller.js';
 
 import mongoose from 'mongoose';
 
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
-    || 'mongodb://localhost:27017//fsd';
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb+srv://Matamir:Skier$Applaud1$Radiance$Distract$Unscented@cluster0.zhzss.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
-mongoose.connect(CONNECTION_STRING);
+mongoose.connect(CONNECTION_STRING)
 
 
 const app = express();
