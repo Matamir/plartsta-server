37f33616590b3544426d11363da7412ace5c70bc
Matamir <mhirsch217@gmail.com>
2022-05-01 12:30:18 -0400

    add instagram api stuff

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -1,14 +1,30 @@
+import axios from "axios";
 import items from "./posts.js"
 let posts = items;
+
+// const POSTS_API = 'http://localhost:4000/api/posts'
+
+// export const findAllPosts = async () => {
+//     const res = await axios.get(POSTS_API);
+//     const posts = res.data;
+//     return posts;
+// }
+
+const IG_API = 'https://graph.instagram.com/7509095509130541/media?fields=id,caption&access_token='
+var api_key = process.env.insta_key;
+const res = await axios.get(IG_API.concat(api_key));
+const posts_raw = res.data;
+
 
 const postController = (app) => {
     app.get('/api/posts', findAllPosts);
     app.get('/api/posts/:uid', findPostById);
+
 }
 
 
 const findAllPosts = (req, res) => {
-    res.json(posts);
+    res.json(post_raw);
 }
 
 const findPostById = (req, res) => {
