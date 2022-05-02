5bbbf3afcb3c23a7d22c20eb26645709b311226f
Matamir <mhirsch217@gmail.com>
2022-05-02 04:04:09 -0400

    redeploy

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -12,7 +12,6 @@ const userController = (app) => {
     app.put('/api/users/:uid', updateUser);
 }
 
-
 const createUser = async (req, res) => {
     const newUser = req.body;
     const insertedUser = await usersDao.createUser(newUser);
