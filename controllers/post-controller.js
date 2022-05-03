65a0f01e3c12dd59fd32451088e49c19da70b6d7
Matamir <mhirsch217@gmail.com>
2022-05-02 22:37:26 -0400

    add createPost

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import postsDao, { createPost } from './Mongoose/posts/posts-dao.js';
+import postsDao from './Mongoose/posts/posts-dao.js';
 
 
 const postController = (app) => {
